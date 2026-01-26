7999334bb63a55e5082bcfe56288bd9fc9659a40
venkatesh s <venkatesh.s2006@gmail.com>
2026-01-26 23:43:42 +0530

    updated with initial commit

---
+++ src/main/java/com/bnpp/datamasking/DatamaskingApplication.java
@@ -0,0 +1,13 @@
+package com.bnpp.datamasking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatamaskingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatamaskingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnpp/datamasking/DatamaskingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnpp.datamasking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatamaskingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
